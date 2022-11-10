dcac17ee9ece9bfbf4c57bfe4bf3a30bb6777bf4
Stanley_Woo <stanleywoo450401@gmail.com>
2022-11-10 12:39:25 -0800

    Update main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -52,8 +52,8 @@ function initializeServiceWorker() {
   // B2. TODO - Listen for the 'load' event on the window object.
   window.addEventListener('load', async () => {
     try {
-      const registration = await navigator.serviceWorker.register("/sw.js", {
-        scope: "",
+      const registration = await navigator.serviceWorker.register("/Lab7_Starter/sw.js", {
+        scope: "/",
       });
       if (registration.installing) {
         console.log("Service worker installing");
